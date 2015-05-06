e20be6f44f2f01f4c2561865f740e080e58faff0
jiangecho <jiangyingji1@163.com>
2015-05-06 19:24:42 +0800

    add QuoteGen

---
+++ QuoteGen/QuoteGen/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QuoteGen
+//
+//  Created by jiangecho on 15/5/6.
+//  Copyright (c) 2015年 jiangecho. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
