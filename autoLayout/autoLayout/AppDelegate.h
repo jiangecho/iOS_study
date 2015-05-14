9152c614fec41232abd2357f18a296a16d0b988b
jiangecho <jiangyingji1@163.com>
2015-05-14 10:22:05 +0800

    learn autolayout

---
+++ autoLayout/autoLayout/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  autoLayout
+//
+//  Created by jiangecho on 15/5/13.
+//  Copyright (c) 2015年 jiangecho. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ autoLayout/autoLayout/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  autoLayout
+//
+//  Created by jiangecho on 15/5/13.
+//  Copyright (c) 2015年 jiangecho. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
